a97bcc0e4f560cbb93b3fc809f49da7af9186f5f
Kiran Bhatt <kb_43@outlook.com>
2019-05-21 10:05:36 +0545

    minor changes

--- app/containers/NewsCategory/selectors.js
+++ app/containers/NewsCategory/selectors.js
@@ -1,27 +1,15 @@
 import { createSelector } from 'reselect';
 import { initialState } from './reducer';
 
-/**
- * Direct selector to the newsCategory state domain
- */
-
 const selectNewsCategoryDomain = state =>
   state.get('newsCategory', initialState);
-
-/**
- * Other specific selectors
- */
-
-/**
- * Default selector used by NewsCategory
- */
 
 const makeSelectNewsCategory = () =>
   createSelector(
     selectNewsCategoryDomain,
     substate => substate.get('newsCategoryResponse'),
   );
-const   makeSelectDeleteNewsCategory = () =>
+const makeSelectDeleteNewsCategory = () =>
   createSelector(
     selectNewsCategoryDomain,
     substate => substate.get('deleteCategoryResponse'),
